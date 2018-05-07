89830588ca81e202e77d12ee714661174c0a759a
gzmkobe <gzmkobe@gmail.com>
2018-05-07 17:01:08 -0400

    Delete reduce_property.py

--- map-reduce/reduce_property.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-for line in sys.stdin:
-        line = line.strip()
-        key, value = line.split('\t',1)
-        print ('{0}\t{1}'.format(key, value))
-          
